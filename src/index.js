790eac0f27db1c44b004bb9b06ddf5f30966ac5d
BaeJuh <ghkslqlsl04@gmail.com>
2024-09-10 16:24:52 +0900

    EJS Test complete

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import mysql from "mysql";
 
 const app = express();
 
-console.log("Hello world");
+app.use( express.static("public") );
+
+app.set("view engine", "ejs");
+app.set("views", "view");
+
+app.get("/", (req, res) => {
+    res.render( "layout", { content: "selectMovie_hour", downBar: "bottomBarFrame" } );
+});
+
+app.listen( 3000, () => {
+    console.log("3000 Port is ready");
+});
